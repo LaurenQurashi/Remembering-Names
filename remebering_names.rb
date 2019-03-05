de2f3f45f6428178aa2a69a91d2311dad2ebe28d
Lauren <laurenqurashi@me.com>
2019-03-05 11:24:37 +0000

    Exercise 1 - Imagine how to use it

---
+++ remebering_names.rb
@@ -0,0 +1,18 @@
+
+
+Brief -
+
+As a student
+So that I can remember people's names
+I want an IRB application that stores the names of people I meet
+
+Step 1 - Imagine how to use it
+  > remember_name("Name")
+  => return "Name Stored!"
+  > show_names
+  => ["Name"]
+  > remember_name("Name2")
+  => return "Name Stored!"
+  > show_names
+  => ["Name", "Name2"]
+  
