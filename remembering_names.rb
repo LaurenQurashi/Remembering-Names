ff3d462d406274ce15cabbe7d55181e9da3f0a81
Lauren <laurenqurashi@me.com>
2019-03-05 11:40:26 +0000

    Exercise 3 - Deciding what software to write

--- remembering_names.rb
+++ remembering_names.rb
@@ -7,12 +7,15 @@ So that I can remember people's names
 I want an IRB application that stores the names of people I meet
 
 Step 1 - Imagine how to use it
-  > remember_name("Name")
-  => return "Name Stored!"
-  > show_names
-  => ["Name"]
-  > remember_name("Name2")
-  => return "Name Stored!"
-  > show_names
-  => ["Name", "Name2"]
-  
+
+describe "the remember_names method" do
+  it "gives us a friendly message" do
+    expect(remember_name("Name")).to eq "Name Stored!"
+  end
+end
+
+describe "the show_names method" do
+  it "shows us the listed names" do
+    expect(show_names).to eq list_names
+  end
+end
